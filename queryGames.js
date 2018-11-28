9ed9ce1f6c1c745a80c29addfc59cd3a342d5482
José Guilherme <guilherme-lima@hotmail.com>
2018-11-27 22:55:46 -0300

    Create queryGames.js

---
+++ queryGames.js
@@ -0,0 +1,61 @@
+import wixData from "wix-data";
+
+$w.onReady(() => {
+  loadCategorias();
+  /*wixData.query('Categorias')
+	  .find()
+	  .then(res => {
+	  	let options = [{"value": '', "label": 'All Categoria'}];
+	  	options.push(...res.items.map(categoria => {
+	  		return {"value": categoria.title, "label": categoria.title};
+	  	}));
+	  	$w('#iCategory').options = options;
+	  });*/
+});
+
+let lastFilterTitle;
+let lastFilterCategoria;
+let debounceTimer;
+export function iTitle_keyPress(event, $w) {
+  if (debounceTimer) {
+    clearTimeout(debounceTimer);
+    debounceTimer = undefined;
+  }
+  debounceTimer = setTimeout(() => {
+    filter($w('#iTitle').value, lastFilterCategoria);  
+  }, 500);
+}
+
+export function iCategoria_change(event, $w) {
+	filter(lastFilterTitle, $w('#iCategory').value);
+}
+
+function filter(title, categoria) {
+  if (lastFilterTitle !== title || lastFilterCategoria !== categoria) {
+    let newFilter = wixData.filter();
+    if (title)
+      newFilter = newFilter.contains('nome', title);
+    if (categoria)
+      newFilter = newFilter.contains('categoria', categoria);
+    $w('#dataset1').setFilter(newFilter);		
+    lastFilterTitle = title; 
+    lastFilterCategoria = categoria;
+  }
+}
+
+function loadCategorias() {
+  wixData.query('Categorias')
+	  .find()
+	  .then(res => {
+	  	let options = [{"value": '', "label": 'All Categoria'}];
+	  	options.push(...res.items.map(categoria => {
+	  		return {"value": categoria.title, "label": categoria.title};
+	  	}));
+	  	$w('#iCategory').options = options;
+	  });
+
+}
+
+export function iCategory_change(event, $w) {
+	filter(lastFilterTitle, $w('#iCategory').value); 
+}
